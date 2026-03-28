9d1220d077f930913e25620b57016a747cf67478
AlucikeP <1745132070@qq.com>
2026-03-28 22:20:51 +0800

    加入剩下几种怪物的代码模板

---
+++ Assets/Scripts/Adventurer/Skeleton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : Adventurer
+{
+    private void CheckArea()
+    {
+        //给对应区域加成
+    }
+
+    private void CheckAreaMouseDown()
+    {
+        //从区域脱离时移除对区域的buff
+    }
+}
---
+++ Assets/Scripts/Adventurer/Troll.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Troll : Adventurer
+{
+    //新单位的制作通过实现加成逻辑，填数据和图片，设置动画即可
+    private void CheckArea()
+    {
+        //给对应区域加成
+    }
+
+    private void CheckAreaMouseDown()
+    {
+        //从区域脱离时移除对区域的buff
+    }
+}
